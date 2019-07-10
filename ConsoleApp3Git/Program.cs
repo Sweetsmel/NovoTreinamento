401d997b21cc50c929b7d6e48404574ecaf51156
felipe junior <contato@usysweb.com.br>
2019-07-10 08:45:31 -0300

    Commit Primeira aula Git

---
+++ ConsoleApp3Git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Git!");
+            Console.ReadLine();
+        }
+    }
+}
