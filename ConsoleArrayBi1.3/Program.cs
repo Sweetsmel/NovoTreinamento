d0dd36bcb623785a2b37da4ed2cafb340851bb8d
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:25 -0300

    Arrais de duas Posições S.

---
+++ ConsoleArrayBi1.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArrayBi1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeLivros = new string[2, 2]
+                {{"O Rei Leão","Sim"},{"Pinóquio","Não"}};
+
+            for (int i = 0; i < listaDeLivros.GetLength(0); i++)
+            {
+                var Nomelivro = listaDeLivros[i,0];             //i = linha e 0 ou 1 = coluna
+                var disponivel = listaDeLivros[i,1];
+
+                Console.WriteLine($"Nome do Livro: {Nomelivro} Disponibilidade: {disponivel}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
