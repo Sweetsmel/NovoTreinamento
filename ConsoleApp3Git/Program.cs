28248a5047c896055d0509b1410bd95d82057a81
John Doe <john.doe@gmail.com>
2019-07-10 10:22:33 -0300

    Mostrando entendimento de hora/data

--- ConsoleApp3Git/Program.cs
+++ ConsoleApp3Git/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp3Git
@@ -10,8 +11,25 @@ namespace ConsoleApp3Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Git!");
-            Console.ReadLine();
+            #region
+            string[] nameList = new string[2] {"Dia","Hora"};
+
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firstTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("dd/MM/yyyy e hh:mm:ss.fff");
+                Console.WriteLine(dateList[i]);
+            }
+
+            Console.WriteLine((DateTime.Now - firstTime).ToString());
+                            
+            Console.ReadKey();
+            #endregion
         }
     }
 }
