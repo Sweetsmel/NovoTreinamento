e1102e3d4d7c28234da6d569c2c5dfacea86db6d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 10:07:43 -0300

    Lista Bidimensional de Maneira Simples. Atualizado

--- ListaBiDeManeiraSimples/Program.cs
+++ ListaBiDeManeiraSimples/Program.cs
@@ -9,10 +9,19 @@ namespace ListaBiDeManeiraSimples
     class Program
     {
         static void Main(string[] args)
-        {                                                                               //criando nossa lista com mais de uma dimensão
+        {                                                                                //criando nossa lista com mais de uma dimensão
             string[,] listaDeNome = new string[5, 2];
-                                                                                        //Aqui como estamos usando apenas uma referencia da lista colocamos "ref" ao passar ela no metodo
+                                                                                         //Aqui como estamos usando apenas uma referencia da lista colocamos "ref" ao passar ela no metodo
             CarregaInformacoesEListaElasEmTela(ref listaDeNome);
+                                                                                         //Após carregar as informações e mostrar em tela ele espera um comando
+            Console.ReadKey();
+                                                                                         //Indicamos que o usuario precisa informar um numero de identificação para pesquisar um registro
+            Console.WriteLine("Informa o ID do registro a ser pesquisado.");
+                                                                                         //Aqui como realizamos a pesquisa somente na chamada, passamos a nossa lista normalmente pois não iremos altear e apenas pesquisar a informação
+                                                                                         //Após a vírgula temos o console readLine que espera nosso identificador unico
+            PesquisandoInformacoesNaNossaLista(listaDeNome, Console.ReadLine());
+
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -36,22 +45,23 @@ namespace ListaBiDeManeiraSimples
                 Console.WriteLine($"ID:{arrayBi[i, 0]} - Nome:{arrayBi[i, 1]}");
             }
 
-            Console.ReadKey();
         }
             /// <summary>
             /// Metodo que realiza a pesquisa pelo identificador unico de nossa coleção
             /// </summary>
             /// <param name="arrayBi">Nossa coleção de informações</param>
             /// <param name="pId">Nosso identificador unico</param>
-        public static void PesquisandoInformacoesNaNossaLista(ref string[,] arrayBi, string pId)
+        public static void PesquisandoInformacoesNaNossaLista(string[,] arrayBi, string pId)
         {
                                                                                         //Realizamos nossa comparação dos mesmos tipos
              for (int i = 0; i < arrayBi.GetLength(0); i++)
              {                                                                          //Mostramos as informações formatadas da nossa pesquisa
-                  if (arrayBi[i, 0] == pId)
-                  Console.WriteLine($"Informação escolhida: Id>{arrayBi[i, 0]} - Nome:{arrayBi[i, 1]}");
-                                                                                        //Aqui saimos da nossa lista mas retornamos vazio return, pois estamos em um metodo vazio void que nao espera retornar algo
-                  return;
+                if (arrayBi[i, 0] == pId)
+                {
+                    Console.WriteLine($"Informação escolhida: Id>{arrayBi[i, 0]} - Nome:{arrayBi[i, 1]}");
+                    //Aqui saimos da nossa lista mas retornamos vazio return, pois estamos em um metodo vazio void que nao espera retornar algo
+                    return;
+                }
              }
 
                                                                                         //Caso ele passe por esta linha identificamos que ele ao encontrou resultados compatíveis
