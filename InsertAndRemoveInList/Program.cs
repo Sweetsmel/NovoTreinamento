505922aa29c3f3b2f76b84799fa0206fd070dbd8
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-16 11:49:44 -0300

    Projetinho Final da Biblioteca finalizado.

--- InsertAndRemoveInList/Program.cs
+++ InsertAndRemoveInList/Program.cs
@@ -11,7 +11,7 @@ namespace InsertAndRemoveInList
         static void Main(string[] args)
         {
             //Criamos a nossa base de dados inicial.
-            string[,] baseDeDados = new string[2, 3];
+            string[,] baseDeDados = new string[2, 5];
 
             //Indicador dos registros realizados em nosso sistema
             int IndiceBaseDados = 0;
@@ -23,7 +23,8 @@ namespace InsertAndRemoveInList
             var escolhaInicial = ApresentaMenuInicial();
 
             while(true)
-            {   //Iniciando a escolha do nosso menu
+            {   
+                //Iniciando a escolha do nosso menu
                 switch (escolhaInicial)
                 {
                     //1-Insere as informações
@@ -32,14 +33,23 @@ namespace InsertAndRemoveInList
                     case "2": { RemoverInformaçoes(ref baseDeDados); } break;
                     //3- Lista as informações da lita
                     case "3": { MostrarInformaçoes(baseDeDados); } break;
+                    //Menu que mostra apenas registros desativados do sistema
+                    case "4": { MostrarInformaçoes(baseDeDados, "true"); } break;
+                    //Return dentro do nosso caso de escolha, ele sai do nosso principal ou metodo que estamos dentro de contexto.
                     //Sai do nosso sistema
-                    case "4": { return; }   //Return dentro do nosso caso de escolha, ele sai do nosso principal ou metodo que estamos dentro de contexto.
-
-
+                    case "5": { return; }
                 }
                 //Alimenta a escolha novamente
                 escolhaInicial = ApresentaMenuInicial();
             }
+        }
+        public static void MostrarInicio()
+        {
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("        BEM VINDO AO SISTEMA DE REGISTRO DE LIVROS!");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("             Desenvolvido pela indústria S.A");
+            Console.WriteLine("-----------------------------------------------------------");
         }
         /// <summary>
         /// Apresenta as informações do menu inicial.
@@ -53,7 +63,8 @@ namespace InsertAndRemoveInList
             Console.WriteLine("\r\n1 - Inserir um novo registro");
             Console.WriteLine("2 - Remover um novo registro");
             Console.WriteLine("3 - Listar informações");
-            Console.WriteLine("4 - Sair do Sistema");
+            Console.WriteLine("4 - Lista as informações desativadas.");
+            Console.WriteLine("5 - Sair do Sistema");
 
             Console.WriteLine("\r\nDigite o número da opção desejada:");
             //Retorna diretamente o menu escolhido.
@@ -91,6 +102,10 @@ namespace InsertAndRemoveInList
                 baseDeDados[i, 1] = nome;
                 //Carregamos na segunda coluna o valor da idade
                 baseDeDados[i, 2] = idade;
+                //Carrega a coluna q identifica se o registro está ativo
+                baseDeDados[i, 3] = "true";
+                //Identificamos agora a data e hora de criação dos registros dentro do sistema
+                baseDeDados[i, 4] = DateTime.Now.ToString("dd/MM/yyyy e HH:mm:ss");
 
                 //Finalizamos aqui para apenas inserir um registro por vez
                 break;
@@ -102,21 +117,29 @@ namespace InsertAndRemoveInList
             Console.ReadKey();
         }
         /// <summary>
-        /// Mostra as informações dentro da nossa lista de dados "base de dados"
+        /// Mostra as informações dentro da nossa lista de dados"base de dados"
         /// </summary>
-        /// <param name="baseDeDados">Base de dados para a leitura e mostrar para o usuario</param>
-        public static void MostrarInformaçoes(string[,] baseDeDados)
+        /// <param name="baseDeDados">base de dados para a leitura e mostrar pro usuario</param>
+        /// <param name="mostrarRegistrosNAtivos">Quando identificado com o valor true, o mesmo
+        /// mostra os valores que não estão ativos dentro do sistema.</param>
+        public static void MostrarInformaçoes(string[,] baseDeDados,string mostrarRegistrosNAtivos = "false") //caso "registrosAtivos" não seja informado será falso
         {
             //Informamos em que tela o mesmo está
             Console.WriteLine("Apresentação das informações dentro da Base de Dados;");
-            
+
+            //Comparação que identifica visualmente quais registros estamos mostrando
+            if (mostrarRegistrosNAtivos == "true")
+                Console.WriteLine("Registros desativados dentro do sistema.");
+
             //Laço simples aonde o mesmo mostra de maneira formatada as informações 
             for (int i = 0; i < baseDeDados.GetLength(0); i++)
             {
-                Console.WriteLine($"ID {baseDeDados[i,0]} " +
-                    $"- Nome: {baseDeDados[i,1]} " +
-                    $"- Idade: {baseDeDados[i,2]}");
-
+                //Aqui deixamos de mostrar as informações que foram desabilitadas dentro do sistema.
+                if(baseDeDados[i,3] != mostrarRegistrosNAtivos)
+                    Console.WriteLine($"ID {baseDeDados[i, 0]} " +
+                                      $"- Nome:{baseDeDados[i, 1]} " +
+                                      $"- Idade:{baseDeDados[i, 2]}" +
+                                      $"- Data Alteração:{baseDeDados[i, 4]}");
             }
             //Finalizamos a operação e indicamos que não existe mais operações a serem realizadas em nosso metodo.
             Console.WriteLine("Resultados apresentados com sucesso!");
@@ -136,13 +159,16 @@ namespace InsertAndRemoveInList
             //Laço de repetição que mostra as informações dentro da tela de exclusão para facilitar a escolha do ID corretamente.
             for (int i = 0; i < baseDeDados.GetLength(0); i++)
             {
+                //Identifica que so deve remover os valores ativos dentro do sistema
+                if (baseDeDados[i,3] != "false")
                 Console.WriteLine($"ID:{baseDeDados[i,0]} " +
                    $"- Nome:{baseDeDados[i,1]} " +
                    $"- Idade:{baseDeDados[i,2]}");
-
             }
+            
             //Indicamos para o usuário informar um ID dentro do nosso sistema para remover
             Console.WriteLine("Informe o ID do registro a ser removido:");
+
             var id = Console.ReadLine();
 
             //Outro laço agora para remover o registro caso o mesmo exista
@@ -152,15 +178,16 @@ namespace InsertAndRemoveInList
                 //Colocamos um "&&" pois a comparação de um valor string com um valor null pode geral erro.
                 if(baseDeDados[i,0] != null && baseDeDados[i,0] == id)
                 {
-                    //Nesta parte limpamos o registro de nossa base de dados
-                    baseDeDados[i, 0] = null;
-                    baseDeDados[i, 1] = null;
-                    baseDeDados[i, 2] = null;
+                    //Agora trocamos este valor para um identificador string "false"
+                    baseDeDados[i, 3] = "false";
+                    //Aqui indicamos a data que foi alterado esse registro.
+                    baseDeDados[i, 4] = DateTime.Now.ToString("dd/MM/yyyy e HH:mm:ss"); //Busca a data/hr do momento que foi realizado a atividade do usuario
                 }
             }
             //Finalizamos as operações desta tela.
             Console.WriteLine("Operações finalizadas.");
             Console.WriteLine("Para retornar ao menu inicial, favor clicar em qualquer tecla.");
+
             Console.ReadKey();
         }
         /// <summary>
@@ -169,35 +196,43 @@ namespace InsertAndRemoveInList
         /// <param name="baseDeDados">Retorna nossa Base de Dados.</param>
         public static void AumentaTamanhoLista(ref string[,] baseDeDados)
         {
+            //Verifica se precisa criar uma lista maior
             var limiteDaLista = false;
 
+            //Laço que verifica se existe registro disponível
             for (int i = 0; i < baseDeDados.GetLength(0); i++)
             {
-
+                //Caso ainda existir registro disponivel ele seta nossa variavel "limiteDaLista" para false
                 if (baseDeDados[i, 0] == null)
                     limiteDaLista = false;
             }
 
+            //Caso não tenha mais registro nossa variavel ficou como true então indica que precisamos aumentar nossa lista
             if (limiteDaLista)
             {
-
+                //criamos uma cópia da nossa lista para não perder os valores
                 var listaCopia = baseDeDados;
 
-                baseDeDados = new string[baseDeDados.GetLength(0) + 5, 3];  //linhas e colunas
+                //Aqui Limpamos nossa lista antigas e assinamos novamente com uma lista com mais espaços
+                baseDeDados = new string[baseDeDados.GetLength(0) + 5, 5];  //linhas e colunas
 
+                //Agora copiamos os registros da nossa lista antiga e passamos para a nossa nova lista
                 for (int i = 0; i < listaCopia.GetLength(0); i++)
                 {
-
+                    //Copiamos a informação deta tela
                     baseDeDados[i, 0] = listaCopia[i, 0];
-
+                    //Copiamos a informação do nosso nome
                     baseDeDados[i, 1] = listaCopia[i, 1];
-
+                    //A informação da idade foi atualizada
                     baseDeDados[i, 2] = listaCopia[i, 2];
+                    //Identificador se o registro está ativo
+                    baseDeDados[i, 3] = listaCopia[i, 3];
+                    //Data da alteração do registro
+                    baseDeDados[i, 4] = listaCopia[i, 4];
                 }
-
+                //indicamos que neste ponto a lista foi atualizada em seu tamanho.
                 Console.WriteLine("O tamanho da lista foi atualizado!");
             }
-
         }
     }
 }
